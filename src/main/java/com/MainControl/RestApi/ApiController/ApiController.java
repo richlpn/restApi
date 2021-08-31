2627e97b051448421abae50f75ddf457374f730f
RichardL <richardl1010@outlook.com>
2021-08-31 16:02:23 -0300

    Version 1.1

---
+++ src/main/java/com/MainControl/RestApi/ApiController/ApiController.java
@@ -0,0 +1,46 @@
+package com.MainControl.RestApi.ApiController;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    public static String productsPath = "src\\main\\resources\\fixtures\\products.txt";
+
+    @GetMapping(value = "/getall")
+    public String getAll(){
+        String Result = "";
+
+        try (Stream<String> stream = Files.lines(Paths.get(productsPath))) {
+            Result += stream.collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Result;
+    }
+
+    @GetMapping(value = "/search")
+    @ResponseBody
+    public String getByFilter(@RequestParam String tag){
+        String Result = "";
+        String tag1 = tag.substring(0, 1).toUpperCase() + tag.substring(1);
+        try (Stream<String> stream = Files.lines(Paths.get(productsPath))) {
+            Result += stream
+                    .filter(Line -> Line.contains(tag1))
+                    .collect(Collectors.toList())
+                    +"\n";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Result;
+    }
+}
--- src/main/java/com/MainControl/RestApi/RestApiApplication.java
+++ src/main/java/com/MainControl/RestApi/RestApiApplication.java
@@ -2,15 +2,12 @@ package com.MainControl.RestApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class RestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
-
 
 }
